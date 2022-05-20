6a13ab73336b42b3fa2c17909942ea75be1123e1
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-20 13:33:51 +0200

    Adding generic list

---
+++ include/list.h
@@ -0,0 +1,20 @@
+#ifndef LUMALANG_LIST_H_
+#define LUMALANG_LIST_H_
+
+#pragma once
+
+class List
+{
+private:
+    void **items;
+    size_t size;
+    size_t item_size;
+
+public:
+    List(size_t size);
+    void list_push(void *item);
+    void *get_items(int index);
+    size_t get_size();
+};
+
+#endif
