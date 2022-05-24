3bf2637ce2fc7a50258b9c1d730bbc495e507cb7
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-24 10:15:16 +0200

    Update tokenizer.h

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -1,6 +1,9 @@
+#ifndef LUMALANG_TOKENIZER_H_
+#define LUMALANG_TOKENIZER_H_
+
 #pragma once
 
-#include "token.h"
+#include <token.h>
 #include <iostream>
 
 class Tokenizer
@@ -10,22 +13,21 @@ private:
     char current;
     int source_size;
     int index;
+ 
 public:
     Tokenizer(std::string source);
 
     void next();
-
     void skip_whitespace();
-
     void skip_comment();
-
+    
     char peek(int buffer);
 
     Token *move_with_token(Token *token);
-
     Token *move_current(Type type);
-
     Token *next_token();
-    Token * parse_id();
-    Token * parse_number();
+    Token *parse_id();
+    Token *parse_number();
 };
+
+#endif
