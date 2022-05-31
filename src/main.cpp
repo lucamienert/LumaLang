25d81ab1da0005f7bee324aa633f8c2c442bbf62
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-31 15:31:41 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include <luma.h>
 
@@ -7,7 +8,8 @@ int main(int argc, char **argv)
     if(argc < 2)
         exit(1);
 
-    compile(argv[1]);
+    if(std::string(argv[1]) == "compile")
+        compile(argv[1]);
 
     return 0;   
 }
