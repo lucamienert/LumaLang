40961b1e6b40dc4ecb847df165c670bb4eb209ae
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-31 15:30:21 +0200

    Uncomment shell_command function

--- src/io.cpp
+++ src/io.cpp
@@ -24,7 +24,7 @@ std::string get_file_content(std::string filename)
 
 std::string shell_command(std::string command)
 {
-    /*std::array<char, 128> buffer;
+    std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command, "r"), pclose);
 
@@ -34,6 +34,6 @@ std::string shell_command(std::string command)
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
         result += buffer.data();
 
-    return result;*/
+    return result;
     return "";
 }
