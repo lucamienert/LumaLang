0820151a49b727840d75422ac344c5d3ad5d1765
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-07-06 16:08:00 +0200

    Move Types to seperate Folder

---
+++ wasm/Types/AbstractSyntaxTreeType.cs
@@ -0,0 +1,15 @@
+﻿namespace Compiler.Types;
+
+public enum AbstractSyntaxTreeType
+{
+    ST_COMPOUND,
+    ST_FUNCTION,
+    ST_CALL,
+    ST_ASSIGNMENT,
+    ST_DEFINITION_TYPE,
+    ST_VARIABLE,
+    ST_STATEMENT,
+    ST_INT,
+    ST_STRING,
+    ST_NOOP
+}
---
+++ wasm/Types/TokenType.cs
@@ -0,0 +1,29 @@
+﻿namespace Compiler.Types;
+
+public enum TokenType
+{
+    TOKEN_ID,
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+    TOKEN_NULL,
+    TOKEN_VAR,
+    TOKEN_FUN,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_COMMA,
+    TOKEN_SEMICOLON,
+    TOKEN_EQUALS,
+    TOKEN_COLON,
+    TOKEN_EOF,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_SLASH,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_WHILE,
+    TOKEN_FOR,
+    TOKEN_RETURN
+}
